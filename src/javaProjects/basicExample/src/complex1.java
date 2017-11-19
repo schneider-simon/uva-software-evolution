c4429e181dc918b17e85fd986934238bf892c42e
Simon Schneider <s.schneider@zwei14.de>
2017-11-19 18:47:27 +0100

    Changed complex1.java

--- src/javaProjects/basicExample/src/complex1.java
+++ src/javaProjects/basicExample/src/complex1.java
@@ -2,6 +2,30 @@ package javaProjects.basicExample.src;
 
 public class complex1 {
 	public void complexity(int test) {
+		int a = 0;
+		int b = 1;
 		
+		if(a+1 > 4) {
+			System.out.println("Impossible");			
+		}
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(b);
+	}
+	
+	public void func2() {
+		int a = 0;
+		int b = 1;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(b);
 	}
 }
